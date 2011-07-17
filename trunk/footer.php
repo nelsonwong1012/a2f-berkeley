1cad34d3d95568db4d68d113a7a33799ebac5010
dkinder.is.me@gmail.com <dkinder.is.me@gmail.com@27463a50-4df5-f9fa-123f-e683710eba0e>
2011-07-17 17:22:35 +0000

    Styling changes

--- trunk/footer.php
+++ trunk/footer.php
@@ -12,8 +12,16 @@
 
 </div><!-- end page -->
 
-<img id="background-image" src="http://farm6.static.flickr.com/5143/5745060215_cff6b90b38_b_d.jpg" alt="" />
+<!-- <img id="background-image" src="http://farm6.static.flickr.com/5143/5745060215_cff6b90b38_b_d.jpg" alt="" /> -->
+<!-- <img id="background-image" src="http://farm3.static.flickr.com/2613/5703947139_ae2d848e6e_b_d.jpg" alt="" /> -->
+<!-- <img id="background-image" src="http://farm4.static.flickr.com/3186/5747297696_57a2fbe3a9_b_d.jpg" alt="" /> -->
+<!-- <img id="background-image" src="http://farm3.static.flickr.com/2388/5745061343_ccf52d581e_b_d.jpg" alt="" /> -->
+<!-- <img id="background-image" src="http://farm4.static.flickr.com/3044/5712668438_4680f1ce2d_b_d.jpg" alt="" /> -->
+<!-- <img id="background-image" src="http://farm3.static.flickr.com/2162/5712106785_fe82873d96_b_d.jpg" alt="" /> -->
+<img id="background-image" src="http://farm4.static.flickr.com/3521/5712104095_341d69ea2b_b_d.jpg" alt="" />
+
 <div id="background-image-overlay"></div>
+<div id="background-texture"></div>
 
 </body>
 </html>
